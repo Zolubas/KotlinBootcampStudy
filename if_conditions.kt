d7db8e9bd2132d08848936b10ad4a597b65dfc22
Fernando Zolubas Preto <49964943+Zolubas@users.noreply.github.com>
2023-03-16 12:42:36 -0300

    Create if_conditions.kt

---
+++ if_conditions.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+        //if conditions
+
+    val x = 1 +  1
+
+    if(x == 2){
+        println("x is 2")
+    } else if( x == 3){
+        println("x is 3")
+    } else {
+        println("x is not 2 or 3")
+    }
+
+        //other way to use if
+    val y = if (x == 2) 2 else 3
+    println("$y")
+}
