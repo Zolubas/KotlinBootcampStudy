ff050c4c714a72405b199bc2f93bb0356be83cbc
Fernando Zolubas Preto <49964943+Zolubas@users.noreply.github.com>
2023-03-17 10:28:01 -0300

    Delete Classes_Animal_And_Dog/kotlin-crash-course directory

--- Classes_Animal_And_Dog/kotlin-crash-course/src/Dog.kt
+++
@@ -1,9 +0,0 @@
-class Dog : Animal("Dog") {
-    init {
-        println("I'm a animal with $legCount legs") //the heir class have the same attributes from mother class (Animal)
-    }
-
-    fun bark(){
-        println("WUUF!!")
-    }
-}
--- Classes_Animal_And_Dog/kotlin-crash-course/src/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-
-        //Classes
-            //Class instantiation
-
-    //val dog = Animal("Dog")
-    val dog = Dog()
-    dog.bark()
-}
